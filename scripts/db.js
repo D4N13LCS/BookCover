93460a5259866ab57e5d97e19ea50fd8138bcc4c
CardosoDaniel <145879116+D4N13LCS@users.noreply.github.com>
2024-10-24 23:48:18 -0300

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,7 +1,7 @@
 const db = require('mysql');
 const pool = db.createPool(
     {"user": "cardosodaniel",
-    "password": process.env.senha,
+    "password": "P3ntatonica#18",
     "host": "db4free.net",
     "port": 3306,
     "database": "l1brary_2"
